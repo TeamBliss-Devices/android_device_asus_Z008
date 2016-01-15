e3e33fb4cfae255b47eb630e35c6180843508bd2
BitO BSessiOn <bitobsession@gmail.com>
2016-01-15 12:30:09 +0100

    Blissified

--- BoardConfig.mk
+++ BoardConfig.mk
@@ -24,3 +24,18 @@ BOARD_USERDATAIMAGE_PARTITION_SIZE := 12478019584
 
 # Use the non-open-source parts, if they're present
 -include vendor/asus/Z008/BoardConfigVendor.mk
+
+# BlissPop Configs
+TARGET_TC_ROM := 4.9
+TARGET_TC_KERNEL := 4.9
+BLISSIFY := true
+BLISS_O3 := false
+BLISS_STRICT := false
+BLISS_GRAPHITE := false
+BLISS_KRAIT := false
+BLISS_PIPE := true
+TARGET_GCC_VERSION_EXP := $(TARGET_TC_ROM)
+TARGET_KERNEL_CUSTOM_TOOLCHAIN := $(TARGET_TC_KERNEL)
+
+#SaberMod
+-include vendor/bliss/config/sm.mk
